5b0f87cfdb5240acadce2e04835251cdbed94350
Jeff Bailey <jbailey@raspberryginger.com>
2026-06-17 12:04:50 +0100

    [libc][NFC] Migrate fchown to syscall wrapper
    Assisted-by: Automated tooling, human reviewed.

---
+++ libc/src/__support/OSUtil/linux/syscall_wrappers/fchown.h
@@ -0,0 +1,32 @@
+//===-- Linux implementation of fchown wrapper ------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_SRC___SUPPORT_OSUTIL_LINUX_SYSCALL_WRAPPERS_FCHOWN_H
+#define LLVM_LIBC_SRC___SUPPORT_OSUTIL_LINUX_SYSCALL_WRAPPERS_FCHOWN_H
+
+#include "hdr/types/gid_t.h"
+#include "hdr/types/uid_t.h"
+#include "src/__support/OSUtil/linux/syscall.h" // For syscall_checked
+#include "src/__support/common.h"
+#include "src/__support/error_or.h"
+#include "src/__support/macros/config.h"
+#include <sys/syscall.h> // For syscall numbers
+
+namespace LIBC_NAMESPACE_DECL {
+namespace linux_syscalls {
+
+#ifdef SYS_fchown
+LIBC_INLINE ErrorOr<int> fchown(int fd, uid_t owner, gid_t group) {
+  return syscall_checked<int>(SYS_fchown, fd, owner, group);
+}
+#endif
+
+} // namespace linux_syscalls
+} // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SRC___SUPPORT_OSUTIL_LINUX_SYSCALL_WRAPPERS_FCHOWN_H
--- libc/src/unistd/linux/fchown.cpp
+++ libc/src/unistd/linux/fchown.cpp
@@ -8,21 +8,17 @@
 
 #include "src/unistd/fchown.h"
 
-#include "src/__support/OSUtil/syscall.h" // For internal syscall function.
+#include "src/__support/OSUtil/linux/syscall_wrappers/fchown.h"
 #include "src/__support/common.h"
-
-#include "hdr/types/gid_t.h"
-#include "hdr/types/uid_t.h"
 #include "src/__support/libc_errno.h"
 #include "src/__support/macros/config.h"
-#include <sys/syscall.h> // For syscall numbers.
 
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(int, fchown, (int fildes, uid_t owner, gid_t group)) {
-  int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_fchown, fildes, owner, group);
-  if (ret < 0) {
-    libc_errno = -ret;
+  auto result = linux_syscalls::fchown(fildes, owner, group);
+  if (!result) {
+    libc_errno = result.error();
     return -1;
   }
   return 0;
