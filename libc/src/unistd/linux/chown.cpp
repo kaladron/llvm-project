1f771010b219df58b754b93c915d60712df1e191
Jeff Bailey <jbailey@raspberryginger.com>
2026-06-29 16:04:31 +0100

    [libc][NFC] Refactor pread, pwrite, chown, fchown to use wrappers
    Migrate pread, pwrite, chown, fchown to use syscall wrappers.
    Add chown, fchown, fchownat, pread, pwrite wrappers.
    
    Assisted-by: Automated tooling, human reviewed.

--- libc/src/unistd/linux/chown.cpp
+++ libc/src/unistd/linux/chown.cpp
@@ -1,35 +1,39 @@
-//===-- Linux implementation of chown -------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+///
+/// \file
+/// Linux implementation of chown.
+///
+//===----------------------------------------------------------------------===//
 
 #include "src/unistd/chown.h"
 
-#include "src/__support/OSUtil/syscall.h" // For internal syscall function.
+#include "src/__support/OSUtil/linux/syscall_wrappers/chown.h"
+#include "src/__support/OSUtil/linux/syscall_wrappers/fchownat.h"
 #include "src/__support/common.h"
 
 #include "hdr/fcntl_macros.h"
 #include "src/__support/libc_errno.h"
 #include "src/__support/macros/config.h"
-#include <sys/syscall.h> // For syscall numbers.
 
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(int, chown, (const char *path, uid_t owner, gid_t group)) {
-#ifdef SYS_chown
-  int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_chown, path, owner, group);
-#elif defined(SYS_fchownat)
-  int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_fchownat, AT_FDCWD, path,
-                                              owner, group, 0);
+#if defined(SYS_fchownat)
+  auto result = linux_syscalls::fchownat(AT_FDCWD, path, owner, group, 0);
+#elif defined(SYS_chown)
+  auto result = linux_syscalls::chown(path, owner, group);
 #else
 #error "chown and fchownat syscalls not available."
 #endif
 
-  if (ret < 0) {
-    libc_errno = -ret;
+  if (!result) {
+    libc_errno = result.error();
     return -1;
   }
   return 0;
